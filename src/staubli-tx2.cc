3932c0e4fb9b6175b8d506d67931fd0605b01172
Florent Lamiraux <florent@laas.fr>
2025-07-04 09:38:52 +0000

    Move code from InverseKinematics to Explicit class.
      This enables us to take into account the right hand side.

--- src/staubli-tx2.cc
+++ src/staubli-tx2.cc
@@ -76,27 +76,73 @@ public:
 				       inConf, outConf, inVel));
   }
 protected:
-  InverseKinematics(const std::string& name, const DevicePtr_t& robot,
-		    const JointConstPtr_t& joint1, const JointConstPtr_t& joint2,
-		    const Transform3s& frame1, const Transform3s& frame2,
-		    const std::string& baseLinkName, const segments_t inConf,
-		    const segments_t outConf, const segments_t inVel) :
+  InverseKinematics(const std::string& name, const DevicePtr_t&,
+		    const JointConstPtr_t&, const JointConstPtr_t&,
+		    const Transform3s&, const Transform3s&,
+		    const std::string&, const segments_t inConf,
+		    const segments_t, const segments_t inVel) :
     DifferentiableFunction(BlockIndex::cardinal(inConf), BlockIndex::cardinal(inVel),
-			   LiegroupSpace::Rn(6), name), robot_(robot), joint1_ (joint1),
-    joint2_ (joint2), frame1_ (frame1), frame2_ (frame2), baseLinkIdx_(
-      robot->model().getFrameId(baseLinkName)),
-    rootIdx_(robot->model().frames[baseLinkIdx_].parentJoint),
-    inConf_(inConf), inVel_(inVel), outConf_(outConf),
-    nq_(robot->model().nq), r1_(0), r2_(0), r3_(0), r4_(0), r5_(0), r6_(0),
-    data_(robot->model()) {
-    q_.resize(robot->configSize());
-    ::pinocchio::neutral(robot->model(), q_.head(nq_));
-    retrieveGeometricalParameters(baseLinkName);
+			   LiegroupSpace::Rn(6), name) {
   }
 
-  virtual void impl_compute(LiegroupElementRef result, vectorIn_t argument) const {
-    forwardKinematics(argument);
-    int extraDof = (int) argument.tail(1)[0];
+  virtual void impl_compute(LiegroupElementRef, vectorIn_t) const {
+    abort();
+  }
+
+  virtual void impl_jacobian(matrixOut_t, vectorIn_t) const {
+    abort();
+  }
+private:
+}; // class InverseKinematics
+
+class Explicit : public constraints::Explicit {
+public:
+  typedef shared_ptr<Explicit> Ptr_t;
+  typedef weak_ptr<Explicit> WkPtr_t;
+  /// Copy object and return shared pointer to copy
+  virtual ImplicitPtr_t copy() const{ return createCopy(weak_.lock()); }
+  /// Create instance and return shared pointer
+  ///
+  /// \param name the name of the constraints,
+  /// \param robot the robot the constraints is applied to,
+  /// \param joint1 the first joint the transformation of which is
+  ///               constrained,
+  /// \param joint2 the second joint the transformation of which is
+  ///               constrained,
+  /// \param frame1 position of a fixed frame in joint 1,
+  /// \param frame2 position of a fixed frame in joint 2,
+  /// \param baseLinkName name of the robot base_link (origin of the robot frame),
+  /// \param extraDof extra degree of freedom used to store an integer. This integrer
+  ///        determines which inverse kinematics solution to return when several exist.
+  /// \note if joint1 is 0x0, joint 1 frame is considered to be the global
+  ///       frame.
+  static Ptr_t create(
+      const std::string& name, const DevicePtr_t& robot,
+      const JointConstPtr_t& joint1, const JointConstPtr_t& joint2, const Transform3s& frame1,
+      const Transform3s& frame2, const std::string& baseLinkName, size_type extraDof) {
+    Explicit* ptr(new Explicit(name, robot, joint1, joint2, frame1, frame2, baseLinkName,
+				   extraDof));
+    Ptr_t shPtr(ptr);
+    WkPtr_t wkPtr(shPtr);
+    ptr->init(wkPtr);
+    return shPtr;
+  }
+
+
+  static Ptr_t createCopy(const Ptr_t& other) {
+    Explicit* ptr(new Explicit(*other));
+    Ptr_t shPtr(ptr);
+    WkPtr_t wkPtr(shPtr);
+    ptr->init(wkPtr);
+    return shPtr;
+  }
+
+  /// Compute output value assuming right hand side is 0.
+  void outputValue(LiegroupElementRef result, vectorIn_t qin, LiegroupElementConstRef rhs) const
+  {
+    assert(rhs == rhs.space()->neutral());
+    forwardKinematics(qin);
+    int extraDof = (int) qin.tail(1)[0];
     // pose of the robot root joint
     Transform3s _0Mb(data_.oMf[baseLinkIdx_]);
     // pose of joint1: J2 * F2 * F1^{-1}
@@ -131,7 +177,7 @@ protected:
     res = div(i, 3);
     int i4 = res.rem;
     int i6 = res.quot;
-    
+
     constexpr double pi = 3.14159265358979323846;
 
     double q1, q2, q3, q4, q5, q6;
@@ -258,10 +304,13 @@ protected:
     result.vector()[4] = q5;
     result.vector()[5] = q6;
   }
-
-  virtual void impl_jacobian(matrixOut_t jacobian, vectorIn_t arg) const {
-    forwardKinematics(arg);
-    J_.resize(6, inVel_.nbIndices());
+  /// Compute Jacobian value assuming right hand side is 0.
+  void jacobianOutputValue(vectorIn_t qin, LiegroupElementConstRef,
+			   LiegroupElementConstRef rhs, matrixOut_t jacobian) const
+  {
+    assert(rhs == rhs.space()->neutral());
+    forwardKinematics(qin);
+    J_.resize(6, BlockIndex::cardinal(inputVelocity()));
     Transform3s _0M1(data_.oMi[joint1_->index()]);
     Transform3s _0M2(data_.oMi[joint2_->index()]);
     Transform3s _0Mr(data_.oMi[rootIdx_]);
@@ -285,21 +334,91 @@ protected:
     ::pinocchio::getJointJacobian(
       robot_->model(), data_, joint2_->index(),::pinocchio::LOCAL, J2_);
     ::pinocchio::getJointJacobian(robot_->model(), data_, rootIdx_, ::pinocchio::LOCAL, Jr_);
-    J2_in_ = inConf_.rview(J2_);
-    Jr_in_ = inConf_.rview(Jr_);
+    J2_in_ = Eigen::RowBlockIndices(inputConf()).rview(J2_);
+    Jr_in_ = Eigen::RowBlockIndices(inputConf()).rview(Jr_);
     J_.topRows(3) = _1R2*(-_2th_cross*J2_in_.bottomRows(3) + J2_in_.topRows(3)) +
       _1Rr*(cross(_rtg)*Jr_in_.bottomRows(3) - Jr_in_.topRows(3)) +
       _0R1*cross(_1tg)*(_1R2*J2_in_.bottomRows(3) - _1Rr*Jr_in_.bottomRows(3));
     J_.bottomRows(3) = _rM2.rotation() * J2_in_.bottomRows(3) - Jr_in_.bottomRows(3);
-    matrix6_t Jout(outConf_.rview(J1_));
+    matrix6_t Jout(Eigen::RowBlockIndices(outputConf()).rview(J1_));
     jacobian = Jout.inverse() * J_;
   }
-private:
+
+ protected:
+  /// Constructor
+  ///
+  /// \param name the name of the constraints,
+  /// \param robot the robot the constraints is applied to,
+  /// \param joint1 the first joint the transformation of which is
+  ///               constrained,
+  /// \param joint2 the second joint the transformation of which is
+  ///               constrained,
+  /// \param frame1 position of a fixed frame in joint 1,
+  /// \param frame2 position of a fixed frame in joint 2,
+  /// \note if joint1 is 0x0, joint 1 frame is considered to be the global
+  ///       frame.
+  Explicit(const std::string& name, const DevicePtr_t& robot,
+	     const JointConstPtr_t& joint1, const JointConstPtr_t& joint2,
+	     const Transform3s& frame1, const Transform3s& frame2, const std::string& baseLinkName,
+	     size_type extraDof)
+    : constraints::Explicit(RelativeTransformationR3xSO3::create(name, robot, joint1, joint2,
+								 frame1, frame2,
+								 std::vector<bool>(6, true)),
+               InverseKinematics::create(name, robot, joint1, joint2,
+	           frame1, frame2, baseLinkName, inputConfVariables(robot, joint1, joint2,
+								    extraDof),
+	           outputConfVariables(joint1), inputVelVariables(robot, joint1, joint2, extraDof)),
+               inputConfVariables(robot, joint1, joint2, extraDof), outputConfVariables(joint1),
+               inputVelVariables(robot, joint1, joint2, extraDof), outputVelVariables(joint1),
+	       ComparisonTypes_t(6*constraints::EqualToZero), std::vector<bool>(6, true)),
+    robot_(robot), joint1_ (joint1),
+    joint2_ (joint2), frame1_ (frame1), frame2_ (frame2), extraDof_(extraDof), baseLinkIdx_(
+      robot->model().getFrameId(baseLinkName)),
+    rootIdx_(robot->model().frames[baseLinkIdx_].parentJoint),
+    nq_(robot->model().nq), r1_(0), r2_(0), r3_(0), r4_(0), r5_(0), r6_(0),
+    data_(robot->model())
+  {
+    // Check that extra-dof is actually an extra configuration space variable, i.e. not a
+    // configuration variable of pinocchio model
+    if (extraDof_ < robot->model().nq) {
+      std::ostringstream os;
+      os << "Extra degree of freedom (" << extraDof << ") should not be a configuration variable "
+	"of the kinematic chain, i.e. should be at least " << robot->model().nq;
+      throw std::logic_error(os.str().c_str());
+    }
+    if (extraDof_ >= robot->configSize()) {
+      std::ostringstream os;
+      os << "Extra degree of freedom (" << extraDof << ") should be smaller than dimension of "
+	"robot configuration space (" << robot->configSize() << ")";
+      throw std::logic_error(os.str().c_str());
+    }
+    q_.resize(robot->configSize());
+    ::pinocchio::neutral(robot->model(), q_.head(nq_));
+    retrieveGeometricalParameters(baseLinkName);
+  }
+
+
+  /// Copy constructor
+  Explicit(const Explicit& other)
+    : constraints::Explicit(other),
+      joint1_(other.joint1_),
+      joint2_(other.joint2_),
+      frame1_(other.frame1_),
+      frame2_(other.frame2_) {}
+
+
+  /// Store weak pointer to itself
+  void init(WkPtr_t weak) {
+    constraints::Explicit::init(weak);
+    weak_ = weak;
+  }
+
+ private:
   // Compute forward kinematics with only input variables
   void forwardKinematics(vectorIn_t arg) const {
-    qsmall_ = inConf_.rview(robot_->currentConfiguration());
+    qsmall_ = Eigen::RowBlockIndices(inputConf()).rview(robot_->currentConfiguration());
     if (qsmall_ != arg) {
-      inConf_.lview(q_) = arg;
+      Eigen::RowBlockIndices(inputConf()).lview(q_) = arg;
     }
     ::pinocchio::forwardKinematics(robot_->model(), data_, q_.head(nq_));
     ::pinocchio::computeJointJacobians(robot_->model(), data_, q_.head(nq_));
@@ -330,13 +449,6 @@ private:
     pinocchio::SE3 j3Mj4(robot_->model().jointPlacements[i4]);
     pinocchio::SE3 j4Mj5(robot_->model().jointPlacements[i5]);
     pinocchio::SE3 j5Mj6(robot_->model().jointPlacements[i6]);
-    hppDout(info, "pMb=" << pMb);
-    hppDout(info, "pMj1=" << pMj1);
-    hppDout(info, "j1Mj2=" << j1Mj2);
-    hppDout(info, "j2Mj3=" << j2Mj3);
-    hppDout(info, "j3Mj4=" << j3Mj4);
-    hppDout(info, "j4Mj5=" << j4Mj5);
-    hppDout(info, "j5Mj6=" << j5Mj6);
     // Check that center of joint1 is on z-axis of base_link
     pinocchio::SE3 bMj1(pMb.inverse()*pMj1);
     // Use a high threshold in case the robot has been calibrated
@@ -357,7 +469,6 @@ private:
     }
 
     pinocchio::SE3 bMj2(bMj1*j1Mj2);
-    hppDout(info, "bMj2=" << bMj2);
     // check that joint2 is oriented like base_link
     if (::pinocchio::log3(bMj2.rotation()).norm() > 1e-3) {
       std::stringstream os;
@@ -400,7 +511,6 @@ private:
     r3_ = j2Mj4.translation()(1);
 
     pinocchio::SE3 j3Mj5(j3Mj4*j4Mj5);
-    hppDout(info, "j3Mj5=" << j3Mj5);
     // check that joint5 is oriented like joint 3
     if (::pinocchio::log3(j3Mj5.rotation()).norm() > 1e-3) {
       std::stringstream os;
@@ -428,24 +538,16 @@ private:
       throw std::logic_error(os.str().c_str());
     }
     r6_ = j5Mj6.translation()(2);
-    hppDout(info, "r1_ = " << r1_);
-    hppDout(info, "r2_ = " << r2_);
-    hppDout(info, "r3_ = " << r3_);
-    hppDout(info, "r4_ = " << r4_);
-    hppDout(info, "r5_ = " << r5_);
-    hppDout(info, "r6_ = " << r6_);
     // Check rotation axes of each joint using Jacobian matrix
     vector_t q(robot_->model().nq);
     ::pinocchio::neutral(robot_->model(), q);
     ::pinocchio::forwardKinematics(robot_->model(), data_, q);
     ::pinocchio::computeJointJacobians(robot_->model(), data_, q);
-    hppDout(info, "data.J = " << data_.J << std::endl);
     vector3_t axis;
     vector3_t y_axis; y_axis << 0, 1, 0;
     vector3_t z_axis; z_axis << 0, 0, 1;
     // joint 1
     axis = data_.J.col(robot_->model().idx_vs[i1]).tail(3);
-    hppDout(info, "axis=" << axis);
     if ((axis-z_axis).norm() > 1e-3) {
       std::stringstream os;
       os << "hpp::inverseKinematics::StaubliTx2::InverseKinematics: joint ("
@@ -455,7 +557,6 @@ private:
     }
     // joint 2
     axis = data_.J.col(robot_->model().idx_vs[i2]).tail(3);
-    hppDout(info, "axis=" << axis);
     if ((axis-y_axis).norm() > 1e-3) {
       std::stringstream os;
       os << "hpp::inverseKinematics::StaubliTx2::InverseKinematics: joint ("
@@ -465,7 +566,6 @@ private:
     }
     // joint 3
     axis = data_.J.col(robot_->model().idx_vs[i3]).tail(3);
-    hppDout(info, "axis=" << axis);
     if ((axis-y_axis).norm() > 1e-3) {
       std::stringstream os;
       os << "hpp::inverseKinematics::StaubliTx2::InverseKinematics: joint ("
@@ -475,7 +575,6 @@ private:
     }
     // joint 4
     axis = data_.J.col(robot_->model().idx_vs[i4]).tail(3);
-    hppDout(info, "axis=" << axis);
     if ((axis-z_axis).norm() > 1e-3) {
       std::stringstream os;
       os << "hpp::inverseKinematics::StaubliTx2::InverseKinematics: joint ("
@@ -485,7 +584,6 @@ private:
     }
     // joint 5
     axis = data_.J.col(robot_->model().idx_vs[i5]).tail(3);
-    hppDout(info, "axis=" << axis);
     if ((axis-y_axis).norm() > 1e-3) {
       std::stringstream os;
       os << "hpp::inverseKinematics::StaubliTx2::InverseKinematics: joint ("
@@ -495,7 +593,6 @@ private:
     }
     // joint 6
     axis = data_.J.col(robot_->model().idx_vs[i6]).tail(3);
-    hppDout(info, "axis=" << axis);
     if ((axis-z_axis).norm() > 1e-3) {
       std::stringstream os;
       os << "hpp::inverseKinematics::StaubliTx2::InverseKinematics: joint ("
@@ -504,15 +601,14 @@ private:
       throw std::logic_error(os.str().c_str());
     }
   }
-
+  // Create LiegroupSpace instances to avoid useless allocation.
   DevicePtr_t robot_;
   JointConstPtr_t joint1_, joint2_;
-  Transform3s frame1_, frame2_;
+  Transform3s frame1_;
+  Transform3s frame2_;
+  size_type extraDof_;
   FrameIndex baseLinkIdx_;
   JointIndex rootIdx_;
-  Eigen::RowBlockIndices inConf_;
-  Eigen::ColBlockIndices inVel_;
-  Eigen::RowBlockIndices outConf_;
   int nq_;
   // Geometrical values retrieved from the model
   value_type r1_, r2_, r3_, r4_, r5_, r6_;
@@ -520,136 +616,7 @@ private:
   mutable vector_t qsmall_, q_;
   mutable ::pinocchio::Data data_;
   mutable matrix_t J_, J1_, J2_, J2_in_, Jr_, Jr_in_;
-}; // class InverseKinematics
-
-class Explicit : public constraints::Explicit {
-public:
-  typedef shared_ptr<Explicit> Ptr_t;
-  typedef weak_ptr<Explicit> WkPtr_t;
-  /// Copy object and return shared pointer to copy
-  virtual ImplicitPtr_t copy() const{ return createCopy(weak_.lock()); }
-  /// Create instance and return shared pointer
-  ///
-  /// \param name the name of the constraints,
-  /// \param robot the robot the constraints is applied to,
-  /// \param joint1 the first joint the transformation of which is
-  ///               constrained,
-  /// \param joint2 the second joint the transformation of which is
-  ///               constrained,
-  /// \param frame1 position of a fixed frame in joint 1,
-  /// \param frame2 position of a fixed frame in joint 2,
-  /// \param baseLinkName name of the robot base_link (origin of the robot frame),
-  /// \param extraDof extra degree of freedom used to store an integer. This integrer
-  ///        determines which inverse kinematics solution to return when several exist.
-  /// \note if joint1 is 0x0, joint 1 frame is considered to be the global
-  ///       frame.
-  static Ptr_t create(
-      const std::string& name, const DevicePtr_t& robot,
-      const JointConstPtr_t& joint1, const JointConstPtr_t& joint2, const Transform3s& frame1,
-      const Transform3s& frame2, const std::string& baseLinkName, size_type extraDof) {
-    Explicit* ptr(new Explicit(name, robot, joint1, joint2, frame1, frame2, baseLinkName,
-				   extraDof));
-    Ptr_t shPtr(ptr);
-    WkPtr_t wkPtr(shPtr);
-    ptr->init(wkPtr);
-    return shPtr;
-  }
-
-
-  static Ptr_t createCopy(const Ptr_t& other) {
-    Explicit* ptr(new Explicit(*other));
-    Ptr_t shPtr(ptr);
-    WkPtr_t wkPtr(shPtr);
-    ptr->init(wkPtr);
-    return shPtr;
-  }
-
-  /// Compute output value assuming right hand side is 0.
-  void outputValue(LiegroupElementRef result, vectorIn_t qin, LiegroupElementConstRef rhs) const
-  {
-    assert(rhs == rhs.space()->neutral());
-    explicitFunction()->value(result, qin);
-  }
-  /// Compute Jacobian value assuming right hand side is 0.
-  void jacobianOutputValue(vectorIn_t qin, LiegroupElementConstRef,
-			   LiegroupElementConstRef rhs, matrixOut_t jacobian) const
-  {
-    assert(rhs == rhs.space()->neutral());
-    explicitFunction()->jacobian(jacobian, qin);
-  }
-
- protected:
-  /// Constructor
-  ///
-  /// \param name the name of the constraints,
-  /// \param robot the robot the constraints is applied to,
-  /// \param joint1 the first joint the transformation of which is
-  ///               constrained,
-  /// \param joint2 the second joint the transformation of which is
-  ///               constrained,
-  /// \param frame1 position of a fixed frame in joint 1,
-  /// \param frame2 position of a fixed frame in joint 2,
-  /// \note if joint1 is 0x0, joint 1 frame is considered to be the global
-  ///       frame.
-  Explicit(const std::string& name, const DevicePtr_t& robot,
-	     const JointConstPtr_t& joint1, const JointConstPtr_t& joint2,
-	     const Transform3s& frame1, const Transform3s& frame2, const std::string& baseLinkName,
-	     size_type extraDof)
-    : constraints::Explicit(RelativeTransformationR3xSO3::create(name, robot, joint1, joint2,
-								 frame1, frame2,
-								 std::vector<bool>(6, true)),
-               InverseKinematics::create(name, robot, joint1, joint2,
-	           frame1, frame2, baseLinkName, inputConfVariables(robot, joint1, joint2,
-								    extraDof),
-	           outputConfVariables(joint1), inputVelVariables(robot, joint1, joint2, extraDof)),
-               inputConfVariables(robot, joint1, joint2, extraDof), outputConfVariables(joint1),
-               inputVelVariables(robot, joint1, joint2, extraDof), outputVelVariables(joint1),
-	       ComparisonTypes_t(6*constraints::EqualToZero), std::vector<bool>(6, true)),
-      joint1_(joint1),
-      joint2_(joint2),
-      frame1_(frame1),
-      frame2_(frame2),
-      extraDof_(extraDof) {
-    // Check that extra-dof is actually an extra configuration space variable, i.e. not a
-    // configuration variable of pinocchio model
-    if (extraDof_ < robot->model().nq) {
-      std::ostringstream os;
-      os << "Extra degree of freedom (" << extraDof << ") should not be a configuration variable "
-	"of the kinematic chain, i.e. should be at least " << robot->model().nq;
-      throw std::logic_error(os.str().c_str());
-    }
-    if (extraDof_ >= robot->configSize()) {
-      std::ostringstream os;
-      os << "Extra degree of freedom (" << extraDof << ") should be smaller than dimension of "
-	"robot configuration space (" << robot->configSize() << ")";
-      throw std::logic_error(os.str().c_str());
-    }
-  }
-
-
-  /// Copy constructor
-  Explicit(const Explicit& other)
-    : constraints::Explicit(other),
-      joint1_(other.joint1_),
-      joint2_(other.joint2_),
-      frame1_(other.frame1_),
-      frame2_(other.frame2_) {}
-
-
-  /// Store weak pointer to itself
-  void init(WkPtr_t weak) {
-    constraints::Explicit::init(weak);
-    weak_ = weak;
-  }
-
- private:
-  // Create LiegroupSpace instances to avoid useless allocation.
-  JointConstPtr_t joint1_, joint2_;
-  Transform3s frame1_;
-  Transform3s frame2_;
-  size_type extraDof_;
   WkPtr_t weak_;
-
 }; // class Explicit
 
 namespace {
